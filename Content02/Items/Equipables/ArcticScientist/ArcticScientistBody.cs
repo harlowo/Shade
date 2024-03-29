83bd348ee4e30d6f3f532ea7852b156383d831dd
Blu3 <124432839+harlowo@users.noreply.github.com>
2024-03-29 14:22:45 +0000

    Add files via upload

---
+++ Content02/Items/Equipables/ArcticScientist/ArcticScientistBody.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using MonoMod;
+using Terraria.Localization;
+
+namespace Shade.Content02.Items.Equipables.Armor.ArcticScientist
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class ArcticScientistBody : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ArmorIDs.Body.Sets.HidesArms[Item.bodySlot] = true;
+            ArmorIDs.Body.Sets.HidesBottomSkin[Item.bodySlot] = true;
+            ArmorIDs.Body.Sets.HidesTopSkin[Item.bodySlot] = true;
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 18;
+            Item.height = 18;
+            Item.defense = 6;
+        }
+        public override void UpdateEquip(Terraria.Player player)
+        {
+            player.GetDamage<MagicSummonHybridDamageClass>() += 0.1f;
+        }
+        public override void AddRecipes()
+        {
+        }
+    }
+}
