cceaa71296c11b455b2272e7bef13c9875cf47c6
Vixxtor69 <139723974+Vixxtor69@users.noreply.github.com>
2024-04-07 15:28:49 -0700

    Update Shade.cs
    added NetEasy registration

--- Shade.cs
+++ Shade.cs
@@ -6,6 +6,15 @@ namespace Shade
     {
         public static Shade Instance;
         public Shade() => Instance = this;
-
+        //this is needed, why wasnt it merged?
+        public override void PostSetupContent()
+        {
+            NetEasy.NetEasy.Register(this);
+        }
+        
+        public override void HandlePacket(BinaryReader reader, int whoAmI)
+        {
+            NetEasy.NetEasy.HandleModule(reader, whoAmI);
+        }
     }
 }
